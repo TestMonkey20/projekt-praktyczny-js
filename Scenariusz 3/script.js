c5c4990c0038a7bc2f63ec951e5ad41551a8169b
TestMonkey20 <aleksander.grodecki@gmail.com>
2021-06-21 00:49:46 +0200

    Refactor

--- Scenariusz 3/script.js
+++ Scenariusz 3/script.js
@@ -1,97 +1,68 @@
 class Product {
-    constructor(n,c,v,q,d) {
-        this.n = n;
-        this.c = c;
-        this.v = v;
-        this.q = q;
-        this.d = d;
+    constructor(name, category, value, quantity, description) {
+        this.name = name;
+        this.category = category;
+        this.value = value;
+        this.quantity = quantity;
+        this.description = description;
     }
 }
 
-let iname = ""
-let icategory = ""
-let ivalue = ""
-let iquantity = ""
-let ides = ""
-let edit = ""
-let l = localStorage.length
 
+/**
+ * Dodaje produkt do tablicy
+ * @param {} product 
+ */
+const pushProduct = (product) => {
+  const klucz = localStorage.getItem('products')
 
-let value = -1
-let display = 0
-let up = -1
+  if(klucz === null) {
+    localStorage.setItem('products', '[]')
+  }
 
-while(display<localStorage.length){
-up++
-let s = JSON.parse(localStorage.getItem("Product" + up))
-
-
-
-  iname += `<li>${s.n}</li>`
-  document.getElementById('iname').innerHTML = iname
-
-  icategory += `<li>${s.c}</li>`
-  document.getElementById('icategory').innerHTML = icategory
-
-  ivalue += `<li>${s.v}</li>`
-  document.getElementById('ivalue').innerHTML = ivalue
-
-  iquantity += `<li>${s.q}</li>`
-  document.getElementById('iquantity').innerHTML = iquantity
-
-  ides += `<li>${s.d}  </li> `
-  document.getElementById('ides').innerHTML = ides
-
-  edit += `<li><button id="delete${value}">usuń</button>  <button id="edition${value}">Edytuj</button> </li> `
-  document.getElementById('edit').innerHTML = edit
-
-display++
-
+  let productArray = JSON.parse(klucz)
+  productArray.push(product)
+  localStorage.setItem('products', JSON.stringify(productArray))
 }
 
 
-value+=up+1
+/**
+ * Usuwa produkt z local storage
+ * @param {} index 
+ */
+const deleteProduct = (index) => {
+    const klucz = localStorage.getItem('products')
 
-btna.addEventListener('click', () =>{
-  
-  value ++
-  
+  if(klucz === null) {
+    return
+  }
 
-  const p1 = new Product(document.getElementById('name').value,
-  document.getElementById('category').value,
-  document.getElementById('value').value +"zł",
-  document.getElementById('quantity').value,
-  document.getElementById('des').value)
- 
+  let productArray = JSON.parse(klucz)
+  productArray.splice(index, 1)
+  localStorage.setItem('products', JSON.stringify(productArray))
+}
 
-  localStorage.setItem('Product'+localStorage.length, JSON.stringify(p1));
+const displayProduct = () => {
+  document.getElementById('iname')
+  document.getElementById('icategory')
+  document.getElementById('ivalue')
+  document.getElementById('iquantity')
+  document.getElementById('ides')
+}
 
-  
-  
-  let j = JSON.parse(localStorage.getItem("Product" +value))
 
-  
-  iname += `<li>${j.n}</li>`
-  document.getElementById('iname').innerHTML = iname
+const addbtn = document.querySelector('.addbtn')
 
-  icategory += `<li>${j.c}</li>`
-  document.getElementById('icategory').innerHTML = icategory
+addbtn.addEventListener('click', () => {
 
-  ivalue += `<li>${j.v}</li>`
-  document.getElementById('ivalue').innerHTML = ivalue
+  const name = document.getElementById('name').value
+  const category = document.getElementById('category').value
+  const value = document.getElementById('value').value
+  const quantity = document.getElementById('quantity').value
+  const des = document.getElementById('des').value
+  const product = new Product(name, category, value, quantity, des)
 
-  iquantity += `<li>${j.q}</li>`
-  document.getElementById('iquantity').innerHTML = iquantity
 
-  ides += `<li>${j.d}</li> `
-  document.getElementById('ides').innerHTML = ides
-
-  edit += `<li> <button id="delete${value}">usuń</button>  <button id="edition${value}">Edytuj</button>  </li> `
-  document.getElementById('edit').innerHTML = edit
-
-  console.log(ides)
-  console.log(edit)
-
+  pushProduct(product)
 })
-
 
